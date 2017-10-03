bf94e58a9cbe09c0be9c3dcaf35034d21e8c8d36
dpodium-roc <32057106+dpodium-roc@users.noreply.github.com>
2017-10-03 17:25:46 +0800

    improe adminhtml
    tasks left:
    ------retrieve info from adminhtml
    ------gather cart info, buyer info etc
    ------fire to pipwave
    ------retrieve token

--- Zamen/CustomPayment/Model/PaymentMethod.php
+++ Zamen/CustomPayment/Model/PaymentMethod.php
@@ -6,4 +6,21 @@ class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
 	//type payment method code here
 	protected $_code='custompayment';
 	
+	//experiment
+	
+	//run all
+	
+	function all()
+	{
+		//check availability
+		//--WRITE HERE----
+		
+		echo 'no';
+		//load language??
+		
+	}
+	all();
+	
+	
+	
 }
